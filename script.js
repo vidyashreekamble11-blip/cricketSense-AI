ed05924c2711ed24617fc7cb2acbef79b7681dc0
Vidyashree Kamble <vidyashreekable11@gmail.com>
2026-08-05 18:45:45 +0530

    Move frontend to root for GitHub Pages

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+console.log("SCRIPT LOADED");
 document.addEventListener("DOMContentLoaded", function () {
 
     const chatInput = document.getElementById("chatInput");
@@ -71,7 +72,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
         chatInput.value="";
 
-        fetch("http://127.0.0.1:5000/chat",{
+        fetch("https://cricketsense-ai.onrender.com/chat",{
             method:"POST",
             headers:{
                 "Content-Type":"application/json"
